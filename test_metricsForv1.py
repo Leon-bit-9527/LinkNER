00f2753780199c51c21e06eb51e115c880eef1f0
ZhenZhang <61865184+Leon-bit-9527@users.noreply.github.com>
2023-05-28 15:45:49 +0800

    Update test_metricsForv1.py

--- test_metricsForv1.py
+++ test_metricsForv1.py
@@ -5,23 +5,11 @@ import time
 # import openai
 from tqdm import tqdm
 
-# openai.api_key = "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"
-
 def save_res(result):
     file = open("ori_results_unc0.1.txt", "a+")
     for data in result:
         line = ' '.join(data) + '\n'
         file.write(line)
-
-# def chatRes(content):
-#     time.sleep(10)
-#     response = openai.ChatCompletion.create(
-#     model = "gpt-3.5-turbo",
-#     temperature = 0.2,
-#     max_tokens = 256,
-#     messages = [{"role": "user", "content":content}]
-#     )
-#     return (response['choices'][0]['message']['content'])
 
 label = "\n Location\n Person\n Organization\n Miscellaneous\n Non-entity"
 label_list = ['LOC','PER','ORG','Non']
@@ -50,32 +38,10 @@ def link(result, th):
                         tmp_label_cnt += 1
                     if str(idx[-6]) != 'O':
                         tmp_pred_cnt += 1
-                    # # print(idx[0])
-                    # # print(float(idx[-2]))
-                    # query = str(context[0]) + "\n Select the entity type of "+ str(idx[0]) +" in this Context, and only need to output the entity type:" + label
-                    # # res = chatRes(query)
-                    # # print(query)
-                    # new_label = [i for i in label_list if i.lower() in res.lower()]
-                    # new_label = ['O'] if new_label == [] else new_label
-                    # new_label = ['O'] if new_label[0] == 'Non' else new_label
-                    # stage1_res = idx[3]
-                    # idx[3] = new_label[0]
-                    # # print(new_label[0])
-                    # # print(idx[2])
-                    # if (str(new_label[0])==str(idx[2])) and (str(stage1_res)!=str(idx[2])):  
-                    #     pos_count += float(idx[-1])
-                    # idx[3] = '<replace>'
                 else:
                     pass
-    
-    #     save_res(result)
-    # else:
-    #     save_res(result)
 
     return unc_count, pos_count, tp, tmp_pred_cnt, tmp_label_cnt     
-    # acc = pos_count/unc_count
-    
-    # print(acc)
 
 def metrics(file_dir):
     print('Threshold: ', 'Precision. ','Recall ','F1 ','Acc ', 'Num. ')
@@ -127,10 +93,6 @@ def metrics(file_dir):
             print(th, precision, recall, f1, pos_count/(unc_count+1e-5),unc_count)
             th += 0.1
             Acc.append(th)
-            
-# metrics('typos8138_result9120.txt')
-# metrics('oov6719_result9120.txt')
-# metrics('ood7818_result9120.txt')
-# metrics('test_result.txt')
+           
 
-metrics('ori_results_unc0.8.txt')
+metrics('')
